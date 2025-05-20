ccd3d07b5f9f1ed57439b8f87e6c608ce14b787c
Anna Guz <3aniaguz@gmail.com>
2025-05-20 11:05:29 +0200

    Plik odpalający program Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import graph_division.division_menu;
+import graph_division.draw_graph;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //division_menu.main(args);
+    }
+}
