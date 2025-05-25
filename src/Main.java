892e01cfeb83f6657c0560b02ad0e2f6a8812a64
Zuzanna Pyzikiewicz <z.pyzikiewicz@gmail.com>
2025-05-25 23:34:14 +0200

    Menu główne

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import graph_division.Graph;
-import graph_division.division_menu;
+import graph_division.MainMenu;
+//import graph_division.division_menu;
 
 
 public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
-        division_menu.main(args, graph);
+        //division_menu.main(args, graph);
+        MainMenu.main(args, graph);
     }
 }
--- src/graph_division/MainMenu.java
+++ src/graph_division/MainMenu.java
@@ -1,15 +1,11 @@
 package graph_division;
 
-import connection_with_C.GraphDividerWrapper;
-
 import javax.swing.*;
 import java.awt.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
+import java.io.File;
 
+public class MainMenu {
 
-
-public class division_menu {
     Color blue = new Color(53, 193, 232);
     Color gray = new Color(180,180,180);
 
@@ -20,51 +16,30 @@ public class division_menu {
     JTextField p_amount_field = new JTextField();
     JTextField margin_field = new JTextField();
 
-    //Powrót
-    public static JPanel create_back_panel(JFrame frame, String[] args, int where_to, Graph graph){
-        JPanel back_panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        back_panel.setBackground(Color.WHITE);
-        JButton back = new JButton("Powrót");
-        back.setFont(new Font("Inter", Font.PLAIN, 20));
-        back.setBackground(new Color(180,180,180));
-        back_panel.add(back);
-        back_panel.setMaximumSize(new Dimension(Integer.MAX_VALUE, back.getPreferredSize().height));
-        back.addActionListener(_ -> {
-                frame.dispose();
-                if(where_to == 1){
-                    division_menu.main(args, graph);
-                }
-                if(where_to == 2){
-                    MainMenu.main(args, graph);
-                }
-        });
-        return back_panel;
-    }
-
     //tytuł
     private void add_caption(JPanel main_panel){
-        JLabel caption = new JLabel("Podział grafu");
+        JLabel caption = new JLabel("Menu główne");
         caption.setFont(new Font("Inter", Font.BOLD, 50));
         caption.setForeground(blue);
         caption.setAlignmentX(Component.CENTER_ALIGNMENT);
         main_panel.add(caption);
     }
 
-    //przyciski "podziel graf", "wyświetl grafy po podziale" i "zapisz grafy po podziale"
+    //przyciski
     private void add_buttons(JPanel main_panel, JFrame frame, String[] args, Graph graph){
-        JButton show_graphs = new JButton("Wyświetl grafy po podziale");
-        show_graphs.setFont(new Font("Inter", Font.PLAIN, 30));
-        show_graphs.setBackground(gray);
-        show_graphs.setAlignmentX(Component.CENTER_ALIGNMENT);
-        Dimension targetSize = show_graphs.getPreferredSize();
+        JButton show_graph = new JButton("Wyświetl graf");
+        show_graph.setFont(new Font("Inter", Font.PLAIN, 30));
+        show_graph.setBackground(gray);
+        show_graph.setAlignmentX(Component.CENTER_ALIGNMENT);
+        Dimension targetSize = show_graph.getPreferredSize();
         targetSize.width += 10;
-        show_graphs.setPreferredSize(targetSize);
-        show_graphs.setMaximumSize(new Dimension(targetSize.width, targetSize.height));
+        show_graph.setPreferredSize(targetSize);
+        show_graph.setMaximumSize(new Dimension(targetSize.width, targetSize.height));
 
-        show_graphs.addActionListener(_ -> {
+        /*show_graph.addActionListener(_ -> {
                 frame.dispose();
                 draw_graph.main(args, graph);
-        });
+        });*/
 
         JButton divide = new JButton("Podziel graf");
         divide.setFont(new Font("Inter", Font.PLAIN, 30));
@@ -74,31 +49,43 @@ public class division_menu {
         divide.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         divide.addActionListener(_ -> {
-            int parts_amount = 2;
-            int margin = 10;
-            if(!p_amount_field.getText().isEmpty()){
-                parts_amount = Integer.parseInt(p_amount_field.getText());
-            }
-            if(!margin_field.getText().isEmpty()){
-                margin = Integer.parseInt(margin_field.getText());
-            }
-            GraphDividerWrapper.main(args, parts_amount, margin);
-            fill_Graph_with_output.main(args, parts_amount, graph);
+        frame.dispose();
+        division_menu.main(args, graph);
         });
 
-        JButton save_graphs = new JButton("Zapisz grafy po podziale");
-        save_graphs.setFont(new Font("Inter", Font.PLAIN, 30));
-        save_graphs.setBackground(gray);
-        save_graphs.setPreferredSize(targetSize);
-        save_graphs.setMaximumSize(new Dimension(targetSize.width, targetSize.height));
-        save_graphs.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton read = new JButton("Wczytaj graf");
+        read.setFont(new Font("Inter", Font.PLAIN, 30));
+        read.setBackground(gray);
+        read.setPreferredSize(targetSize);
+        read.setMaximumSize(new Dimension(targetSize.width, targetSize.height));
+        read.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        //to edit!!!
+        read.addActionListener(_ -> {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(frame);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            //kod wczytywania pliku do grafu
+            File selectedFile = fileChooser.getSelectedFile();
+            System.out.println("Wczytano plik: " + selectedFile.getAbsolutePath());
+            }
+        });
+
+        JButton save_graph = new JButton("Zapisz graf");
+        save_graph.setFont(new Font("Inter", Font.PLAIN, 30));
+        save_graph.setBackground(gray);
+        save_graph.setPreferredSize(targetSize);
+        save_graph.setMaximumSize(new Dimension(targetSize.width, targetSize.height));
+        save_graph.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        main_panel.add(Box.createRigidArea(new Dimension(0, 20)));
+        main_panel.add(read); 
+        main_panel.add(Box.createRigidArea(new Dimension(0, 20)));
+        main_panel.add(show_graph);
         main_panel.add(Box.createRigidArea(new Dimension(0, 20)));
         main_panel.add(divide);
         main_panel.add(Box.createRigidArea(new Dimension(0, 20)));
-        main_panel.add(show_graphs);
-        main_panel.add(Box.createRigidArea(new Dimension(0, 20)));
-        main_panel.add(save_graphs);
+        main_panel.add(save_graph);
     }
 
 
@@ -156,42 +143,8 @@ public class division_menu {
         main_panel.add(toggle_panel);
     }
 
-    //parametry
-    private void create_bottom_panel(JPanel main_panel){
-        JPanel bottom_panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        bottom_panel.setBackground(gray);
-
-        JLabel parameters = new JLabel("Parametry podziału:   ");
-        parameters.setFont(new Font("Inter", Font.PLAIN, 25));
-        bottom_panel.add(parameters);
-
-        JLabel parts_amount = new JLabel("Liczba części: ");
-        parts_amount.setFont(new Font("Inter", Font.PLAIN, 25));
-        bottom_panel.add(parts_amount);
-
-        p_amount_field.setFont(new Font("Inter", Font.PLAIN, 25));
-        p_amount_field.setColumns(2);
-        bottom_panel.add(p_amount_field);
-
-        JLabel margin = new JLabel(" Margines: ");
-        margin.setFont(new Font("Inter", Font.PLAIN, 25));
-        bottom_panel.add(margin);
-
-        margin_field.setFont(new Font("Inter", Font.PLAIN, 25));
-        margin_field.setColumns(2);
-        bottom_panel.add(margin_field);
-
-        JLabel percentage = new JLabel("%");
-        percentage.setFont(new Font("Inter", Font.PLAIN, 25));
-        bottom_panel.add(percentage);
-
-        bottom_panel.setMaximumSize(new Dimension(Integer.MAX_VALUE, (margin_field.getPreferredSize().height)+1000));
-
-        main_panel.add(bottom_panel);
-    }
-
     //konstruktor
-    public division_menu(String[] args, Graph graph) {
+    public MainMenu(String[] args, Graph graph) {
         JFrame frame = new JFrame("GraphDivider");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
@@ -213,18 +166,16 @@ public class division_menu {
         main_panel.setLayout(new BoxLayout(main_panel, BoxLayout.Y_AXIS));
         main_panel.setBackground(Color.WHITE);
 
-        JPanel back_panel = create_back_panel(frame, args, 2, graph);
-        main_panel.add(back_panel);
         add_caption(main_panel);
         add_buttons(main_panel, frame, args, graph);
 
         create_toggle_panel(main_panel);
-        create_bottom_panel(main_panel);
 
         frame.add(main_panel);
         frame.setVisible(true);
     }
     public static void main(String[] args, Graph graph) {
-        SwingUtilities.invokeLater(() -> new division_menu(args, graph));
+        SwingUtilities.invokeLater(() -> new MainMenu(args, graph));
     }
 }
+
